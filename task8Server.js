61889fad4e82380abe5a3d2af13a11171625c0d3
Swati Singh <swatisingh9093@gmail.com>
2023-09-19 13:37:56 +0530

    change in server

--- task8Server.js
+++ task8Server.js
@@ -20,8 +20,50 @@ let connData={
     password:'',
     database:'training',
 };
+const {Client}=require("pg");
+const client=new Client({
+    user:"postgres",
+    password:"Codingisnothing@123",
+    database:"postgres",
+    port:5432,
+    host:"db.hjpthlfsvksrkkeycgvh.supabase.co",
+    ssl:{rejectUnauthorized:false},
+});
+client.connect(function(res,error){
+    console.log('Connected!||');
+});
+app.get("/users",function(req,res,next){
+    console.log("Inside /users get api");
+    const query=`SELECT * FROM users`;
+    client.query(query,function(err,result){
+        if(err){res.status(400).send(err);}
+        res.send(result.rows);
+        client.end();
+    })
+});
+app.post("/user",function(req,res,next){
+    console.log("Inside post of user");
+    var values=Object.values(req.body);
+    console.log(values);
+    const query=`INSERT INTO users (email,firstName,lastName,age) VALUES($1,$2,$3,$4)`;
+    client.query(query,values,function(err,result){
+        if(err){res.send(400).send(err);}
+        res.send(`${result.rowCount}insertion succesful`);
+    })
+})
+app.put("/user/:id",function(req,res,next){
+    console.log("Inside put of user");
+    let userId=req.params.id;
+    let age=req.body.age;
+    let values=[age,userId]
+    const query=`UPDATE users SET age=$1 WHERE id=$2`;
+    client.query(query,values,function(err,result){
+        if(err){res.send(400).send(err);}
+        res.send(`${result.rowCount} updation succesful`);
+    })
+})
 
-const port=2410;
+var port=process.env.PORT||2410;
 app.listen(port,()=>console.log(`Node app listening on port ${port}!`));
 app.get("/srv/emp",function(req,res){
     let department=req.query.department;
